870d4d0e97f8d8d0f3dfd676465928ca8827f28c
Oleksii Remizov <oleksii.remizov@synapse.com>
2017-09-27 17:35:48 +0300

    Second commit

--- GetHashCodeExample/Program.cs
+++ GetHashCodeExample/Program.cs
@@ -6,37 +6,65 @@ using System.Threading.Tasks;
 
 namespace GetHashCodeExample
 {
+    //GetHashCode возвращает одинаковые значения хеш-кодов, но при первоначальном сравнении 
+    //c помощью переопределенного метода Equals "point" и "point1" мы получаем false
+    //После того, как поменяли значение "point1" получаем при сравнении true, что означает равенство 
     class Program
     {
         static void Main(string[] args)
         {
-            GetHashCodeExClass getHashCodeEx1 = new GetHashCodeExClass();
-            GetHashCodeExClass getHashCodeEx2 = new GetHashCodeExClass();
-            Console.WriteLine(getHashCodeEx1.GetHashCode());
-            Console.WriteLine(getHashCodeEx2.GetHashCode());
-            Console.WriteLine();
 
-            GetHashCodeExStruct struct1 = new GetHashCodeExStruct();
-            GetHashCodeExStruct struct2 = new GetHashCodeExStruct();
-            Console.WriteLine(struct1.GetHashCode());
-            Console.WriteLine(struct2.GetHashCode());
+
+            Point point = new Point(1, 11);
+            Console.WriteLine(point.GetHashCode());
+
+            Point point1 = new Point(11, 1);
+            Console.WriteLine(point1.GetHashCode());
+            Console.WriteLine(point1.Equals(point));
+
+            if (point1.Equals(point) == false)
+            {
+                Console.WriteLine("Different types");
+            }
+            else Console.WriteLine("The same types");
+
+            point1 = new Point(1,11);
+            if (point1.Equals(point) == false)
+            {
+                Console.WriteLine("Different types");
+            }
+            else Console.WriteLine("The same types");
+  
+
             Console.ReadKey();
 
         }
     }
 
-    public class GetHashCodeExClass
-    {
-        int x = 3;
-        string s = "String";
-        object o;
-    }
 
-    public struct GetHashCodeExStruct
-    {
-        int x;
-        string s;
-        object o;
 
+    public struct Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point)) return false;
+
+            Point p = (Point)obj;
+            return x == p.x & y == p.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ y.GetHashCode();
+        }
     }
 }
